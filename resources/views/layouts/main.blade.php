72f4d1de69ac33b49dedf697a5b3027f06bc4d4b
seraj.alam <seraj.alam@wisdmlabs.com>
2022-06-18 18:18:16 +0530

    final commit

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -35,10 +35,10 @@ Author URL: http://w3layouts.com
                             <a class="nav-link active" aria-current="page" href="index.html">Home</a>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link" href="courses.html">Courses</a>
+                            <a class="nav-link" href="#">Projects</a>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link" href="contact.html">Contact</a>
+                            <a class="nav-link" href="contact">Contact</a>
                         </li>
                     </ul>
                     <!-- search-right -->
